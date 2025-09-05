484b2a2030baf42763d1133206fa4a7d46d56a4c
MikeHorn-git <MikeHornProton@proton.me>
2025-09-05 20:01:33 +0200

    Improve devenv

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,7 @@
 
 {
   # https://devenv.sh/packages/
-  packages = [ pkgs.git pkgs.compiledb ];
+  packages = with pkgs; [ git checkmake compiledb mdformat nixfmt-classic ];
 
   # https://devenv.sh/languages/
   languages.c.enable = true;
@@ -12,11 +12,17 @@
     "compiledb:run".exec = "compiledb -n make tablet";
     "lint:run".exec = ''
       git ls-files --cached --others --exclude-standard '*.c' '*.h' | xargs clang-format -i
-       clang-tidy tablet.c src/include/syscall.h -- -Isrc -Isrc/include -DTARGET_ARCH_X86_64
-       git ls-files --cached --others --exclude-standard 'Makefile'
-       mdformat README.md
-       git ls-files --cached --others --exclude-standard '*.nix' | xargs nixfmt'';
+      clang-tidy tablet.c src/include/syscall.h -- -Isrc -Isrc/include -DTARGET_ARCH_X86_64
+      checkmake Makefile
+      mdformat README.md
+      nixfmt devenv.nix'';
   };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    make all
+    make tablet
+  '';
 
   # https://devenv.sh/git-hooks/
   git-hooks.excludes = [ ".devenv" ];
